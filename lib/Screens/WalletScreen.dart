b42c96d536c78de29d4ff87aac0a82b2d69f54a4
AyusHSahU1506 <ayushsahu.programmics@gmail.com>
2023-09-25 12:48:31 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 5 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   lib/Screens/WalletScreen.dart

--- lib/Screens/WalletScreen.dart
+++ lib/Screens/WalletScreen.dart
@@ -8,6 +8,7 @@ class MyWallet extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       child: Scaffold(
+        backgroundColor: const Color(0xffffe7dc),
         body: SingleChildScrollView(
           child: Column(
             children: [
@@ -19,6 +20,173 @@ class MyWallet extends StatelessWidget {
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(50),
                         bottomRight: Radius.circular(50))),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  // height: 200.h,
+                  width: MediaQuery.of(context).size.width * 1.w,
+                  decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 3.0,
+                            offset: Offset(
+                              0.0,
+                              5.0,
+                            ))
+                      ],
+                      color: Color(0xfff6f1e7),
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  // height: 200.h,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Wallet Bal.",
+                              style: TextStyle(
+                                  fontFamily: 'Inder',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            ),
+                            Text("Rs. 0",
+                                style: TextStyle(
+                                    fontFamily: 'Inder',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20))
+                          ],
+                        ),
+                        Divider(
+                          thickness: .5,
+                          color: Colors.grey,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Gift Card Bal.",
+                                style: TextStyle(
+                                    fontFamily: 'Inder',
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20)),
+                            Text("Rs. 0",
+                                style: TextStyle(
+                                    fontFamily: 'Inder',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20))
+                          ],
+                        ),
+                        Divider(
+                          thickness: .5,
+                          color: Colors.grey,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Credits",
+                                style: TextStyle(
+                                    fontFamily: 'Inder',
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20)),
+                            Text("Rs. 0",
+                                style: TextStyle(
+                                    fontFamily: 'Inder',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20))
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: .5,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      "  GIFT CARDS  ",
+                      style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: .5,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  // height: 200.h,
+                  width: MediaQuery.of(context).size.width * 1.w,
+                  decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 3.0,
+                            offset: Offset(
+                              0.0,
+                              5.0,
+                            ))
+                      ],
+                      color: Color(0xfff6f1e7),
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  // height: 200.h,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: Column(
+                      children: [
+                        Container(height: 100),
+                        const Divider(
+                          thickness: .5,
+                          color: Colors.grey,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 150,
+                          decoration: const BoxDecoration(
+                              color: Colors.red,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30))),
+                          child: const Center(
+                            child: Text(
+                              "BUY NOW",
+                              style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 23,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
